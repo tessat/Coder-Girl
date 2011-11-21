186f7820af71438d4d4e30449b58f52636d4a5f2
Tessa Taylor <tessa.a.taylor@gmail.com>
2011-11-21 14:14:53 -0500

    Initial commit

---
+++ app/assets/javascripts/pages.js
@@ -0,0 +1,33 @@
+// ***************
+// Includes
+// ***************
+
+// ***************
+// Events
+// ***************
+
+// On index load
+$('.pages.index').ready(function() {
+	run_hello_world();
+});
+
+
+
+// ***************
+// Functions
+// ***************
+
+function run_hello_world() {
+	// Create the hello world element
+	$('.pages.index').append('<div id="page-overlay"><span class="hello-world">\><span class="cursor">&nbsp;</span></span></div>');
+	// Run
+	hello_world("hello world");
+	
+	// On complete
+	$('.hello-world').live('helloWorldDone', function() {
+		$('#page-overlay').fadeOut(1000, function() {
+			$('.hello-world .cursor').die('cursorDone');
+			$('#page-overlay').remove();
+		});
+	});
+}
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Codergirl::Application.config.secret_token = 'f2b9833ecef31453a17ba796c9a869a5587a621bada987dff2fcd18ef84c6b4e76132d0fa2d4aa3c756f981f83a6a25bc23eae81ea6c82c0a29e2d46d1df2127'
